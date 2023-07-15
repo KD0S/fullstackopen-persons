3acbde3a285e71fd207474da6ae018a15a03c20f
Krishnadas Nair <Krishnadas.nair02@gmail.com>
2023-07-15 23:57:03 +0400

    changes

--- index.js
+++ index.js
@@ -18,16 +18,7 @@ const errorHandler = (error, request, response, next) => {
 
 
 app.use(express.json())
-app.use(morgan((tokens, req, res)=>{
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        JSON.stringify(req.body)
-      ].join(' ')
-}))
+app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('build'))
 app.use(errorHandler)
